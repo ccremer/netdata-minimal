0465f62d8c5919fd06b8cc8f028b7ef990cfa98a
Chris <github.account@chrigel.net>
2018-02-18 17:33:29 +0100

    Trying to fix basename error.

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 for file in /etc/netdata/overrides/*.ini; do
-    f=$(basename ${file%.*})
-    echo "Overriding /etc/netdata/${f}.conf with /etc/netdata/overrides/${f}.ini"
-    crudini --inplace --merge /etc/netdata/${f}.conf < /etc/netdata/overrides/${f}.ini
+    f="$(basename ${file%.*})"
+    dest="/etc/netdata/${f}.conf"
+    src="/etc/netdata/overrides/${f}.ini"
+    echo "Overriding ${dest} with ${src}"
+    crudini --inplace --merge ${dest} < ${src}
 done
 
 for file in /etc/netdata/overrides/*.json; do
-    f=$(basename ${file%.*})
+    f="$(basename ${file%.*})"
     dest="/etc/netdata/${f}.conf"
     src="/etc/netdata/overrides/${f}.json"
     echo "Overriding ${dest} with ${src}"
