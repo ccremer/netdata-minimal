b38e7dfc297f45d1de68d15ff95d0548db803b18
Chris <github.account@chrigel.net>
2018-11-04 21:32:23 +0100

    Remove spaces around variables

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ REPOSITORY="${REPOSITORY:-netdata-minimal}"
 
 fail_on_error() {
     if [ ${1} -ne 0 ]; then
-        echo "${2}"
+        echo "------------------------ ${2}"
         echo "Exiting with exit code ${1}"
         exit ${1}
     fi
@@ -22,14 +22,14 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Build images
 for ARCH in armhf amd64 i386 aarch64; do
-    image = "${REPOSITORY}:${ARCH}${VERSION}"
+    image="${REPOSITORY}:${ARCH}${VERSION}"
     echo "Building: ARCH=${ARCH}, tag=${image}"
     docker build --build-arg ARCH="${ARCH}" --tag ${image}
     fail_on_error $? "Error building ${image}, check the output above!"
 done
 
 # Tag latest
-image = "${REPOSITORY}:latest"
+image="${REPOSITORY}:latest"
 echo "Tag latest: ${REPOSITORY}:amd64"
 docker tag "${REPOSITORY}:amd64" "${image}"
 fail_on_error $? "Error tagging ${image}, check the output above!"
@@ -47,14 +47,14 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Push images
 for ARCH in amd64 armhf i386 aarch64; do
-    image = "${REPOSITORY}:${ARCH}${VERSION}"
+    image="${REPOSITORY}:${ARCH}${VERSION}"
     echo "Pushing image: ${image}"
     docker push ${image}
     fail_on_error $? "Error pushing ${image}, check the output above!"
 done
 
 # Push latest
-image = "${REPOSITORY}:latest"
+image="${REPOSITORY}:latest"
 echo "Pushing image: ${image}"
 docker push ${image}
 fail_on_error $? "Error pushing ${image}, check the output above!"
